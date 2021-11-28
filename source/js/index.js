cbfe46db70e08470be1377131a0219df9c9481da
can-dy-jack <jjack_chen3@163.com>
2021-11-28 19:31:47 +0800

    小组件—今日诗词🅿️
    - [ ] 底部添加网站运营时间
    - [ ] 小组件——今日诗词

--- source/js/index.js
+++ source/js/index.js
@@ -1,25 +1,27 @@
-// 为代码块 添加复制按钮
+/**
+ * 为代码块 添加复制按钮
+ */
 const pre = document.querySelectorAll('figure td.code');
 
-pre.forEach(function(item){
+pre.forEach(function (item) {
     // copy button dom
     span = document.createElement('span');
     span.className = 'code-span';
     spanImg = document.createElement('img');
-    spanImg.setAttribute('src','https://z3.ax1x.com/2021/11/17/I4qDrd.png');
-    spanImg.setAttribute('alt','copy');
+    spanImg.setAttribute('src', 'https://z3.ax1x.com/2021/11/17/I4qDrd.png');
+    spanImg.setAttribute('alt', 'copy');
     span.appendChild(spanImg);
 
-    span.setAttribute('data-toggle','tooltip');
-    span.setAttribute('data-placement','left');
-    span.setAttribute('title','点击复制');
+    span.setAttribute('data-toggle', 'tooltip');
+    span.setAttribute('data-placement', 'left');
+    span.setAttribute('title', '点击复制');
 
     // 复制功能
     // item.innerText可以直接获取pre内的文本！！！
-    span.addEventListener('click',()=>{
+    span.addEventListener('click', () => {
         navigator.clipboard
             .writeText(item.innerText)
-            .then(()=>{
+            .then(() => {
                 console.log('copy success!');
             })
     })
@@ -34,3 +36,30 @@ $('.code-span').tooltip();
 // siderbar-icon
 $('.icon-box').tooltip();
 
+/**
+ * 网站运行时间计算 - 函数
+ *
+ * getTime()返回从'1970-1-1 00:00:00 UTC'到该日期对象（该日期对象的UTC时间）的毫秒数
+ * 
+ * 1s = 1000ms
+ */
+var now = new Date();
+function calculateTime(startTime) {
+    now.setTime(now.getTime() + 1000);
+    stime = new Date(startTime);
+    interval = now.getTime() - stime.getTime();
+    days = interval/1000/60/60/24;
+    dnum = Math.floor(days);
+    hours = interval/1000/60/60 - 24*dnum;
+    hnum = Math.floor(hours);
+    if(hnum.toString().length == 1){
+        hnum = '0' + hnum;
+    }
+    minutes = interval/1000/60 - 24*60*dnum - hnum*60;
+    mnum = Math.floor(minutes);
+    if(mnum.toString().length == 1){
+        mnum = '0' + mnum;
+    }
+    document.getElementById("times").innerHTML =
+        "本站已运行 " + dnum + " 天 " + hnum + " 小时 " + mnum + " 分，共" + Math.floor(interval/1000/60/60)+ "小时";
+}
