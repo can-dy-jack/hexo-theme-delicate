beb428fe154c5b588fd8694c7271836ef465b217
can-dy-jack <jjack_chen3@163.com>
2023-07-08 17:22:46 +0800

    version four init

--- scripts/tags/search.js
+++
@@ -1,7 +0,0 @@
-hexo.extend.tag.register('search', (args) => {
-    if(args[1] === undefined) {
-        args[1] = "https://www.baidu.com/s?wd=" + args[0];
-    }
-    return `<a href="${args[1]}" class="search">${args[0]}</a>`
-}, { ends: false });
-// {% search text link %}
