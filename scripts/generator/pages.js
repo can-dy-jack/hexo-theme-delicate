cac61881dd89ba7bd6fc33a6bcb07d1ca34e50a8
can-dy-jack <jjack_chen3@163.com>
2023-07-15 09:48:25 +0800

    category支持

--- scripts/generator/pages.js
+++ scripts/generator/pages.js
@@ -6,11 +6,22 @@
 
 // generate tags page
 hexo.extend.generator.register("_tags", function (locals) {
-  if (this.theme.config.index.links.tag) {
+  if (this.theme.config.index.links.tags) {
     return {
       path: "tags/index.html",
       data: locals.theme,
       layout: "tags",
+    };
+  }
+});
+
+// generate categories page
+hexo.extend.generator.register("_categories", function (locals) {
+  if (this.theme.config.index.links.categories) {
+    return {
+      path: "categories/index.html",
+      data: locals.theme,
+      layout: "categories",
     };
   }
 });
