c3c3f3bf0c796c95a575d2e4241aef3dc7e965ea
can-dy-jack <jjack_chen3@163.com>
2023-07-16 11:27:29 +0800

    样式优化

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,3 @@
-function toTop() {
-    window.scrollTo(0, 0);
-}
-
 const delicateDom = {
     $: str => document.querySelectorAll(str),
     id: str => document.getElementById(str),
@@ -9,8 +5,6 @@ const delicateDom = {
     dom: function() {
         return {
             modeBtn: this.id("mode-btn"),
-            modeBtnIconSun: this.id("mode-btn-icon-sun"),
-            modeBtnIconMoon: this.id("mode-btn-icon-moon"),
             body: document.body || this.$("body")[0],
         }
     },
@@ -30,38 +24,28 @@ const delicate = {
             delicateDom.dom()
         );
         // 事件绑定
-        this.doms.modeBtn.addEventListener("click", (e) => {
-            e.preventDefault();
-            root.toggleMode();
-        });
+        // this.doms.modeBtn.addEventListener("click", (e) => {
+        //     // e.preventDefault();
+        //     root.toggleMode();
+        // });
     },
     changeMode: function(type) {
         // dark to light
         if (type === 'dark') {
             this.doms.body.classList.add('dark');
-            this.doms.modeBtnIconMoon.style.display = "none";
-            this.doms.modeBtnIconSun.style.display = "inline-block";
         } else { // light to dark
             this.doms.body.classList.remove('dark');
-            this.doms.modeBtnIconMoon.style.display = "inline-block";
-            this.doms.modeBtnIconSun.style.display = "none";
         }
     },
     toggleMode: function() {
-        if (this.doms.body.classList.contains('dark')) {
-            this.doms.modeBtnIconMoon.style.display = "inline-block";
-            this.doms.modeBtnIconSun.style.display = "none";
-        } else {
-            this.doms.modeBtnIconMoon.style.display = "none";
-            this.doms.modeBtnIconSun.style.display = "inline-block";
-        }
         this.doms.body.classList.toggle('dark');
     },
-    toTop: toTop,
+    toTop: function() {
+        window.scrollTo(0, 0);
+    },
 }
 
 window.delicate = delicate;
 
 // 初始化
 delicate.init();
-// delicate.changeMode();
